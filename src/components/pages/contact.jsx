67c5f091db1e30addd14abe9ea0a392ce5996ca0
AbduQahhor <abduqahhor.muhamadjonov@gmail.com>
2022-01-16 19:59:29 +0500

    add target for anchor tags

--- src/components/pages/contact.jsx
+++ src/components/pages/contact.jsx
@@ -22,7 +22,7 @@ export const Contact = () => {
             <List>
               <ListItem threeLine>
                 <ListItemContent>
-                  <a href='mailto@abduqahhor.muhamadjonov@gmail.com'>
+                  <a href='mailto:abduqahhor.muhamadjonov@gmail.com' target='_blank' rel='noreferrer'>
                     <i className='contact-page__icon fa fa-envelope-square' />
                     <p className='contact-page__list-item-text'>abduqahhor.muhamadjonov@gmail.com</p>
                   </a>
@@ -30,7 +30,7 @@ export const Contact = () => {
               </ListItem>
               <ListItem threeLine>
                 <ListItemContent>
-                  <a href='https://www.linkedin.com/in/abduqahhor-muhamadjonov'>
+                  <a href='https://www.linkedin.com/in/abduqahhor-muhamadjonov' target='_blank' rel='noreferrer'>
                     <i className='fa fa-linkedin-square contact-page__icon' />
                     <p className='contact-page__list-item-text'>Follow me on LinkedIn</p>
                   </a>
@@ -38,7 +38,7 @@ export const Contact = () => {
               </ListItem>
               <ListItem threeLine>
                 <ListItemContent>
-                  <a href='https://github.com/JKR007'>
+                  <a href='https://github.com/JKR007' target='_blank' rel='noreferrer'>
                     <i className='fa fa-github-square contact-page__icon' />
                     <p className='contact-page__list-item-text'>Watch me on GitHub</p>
                   </a>
